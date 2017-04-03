78abe3dc4a23fa9d980101b016337cca2302e056
youyushangshangqian <281011701@qq.com>
2017-04-03 18:58:13 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,16 +19,16 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.save
 
-      redirect_to groups_path
-    end
+    redirect_to groups_path
+  end
 
-     def update
-       @group = Group.find(params[:id])
+   def update
+     @group = Group.find(params[:id])
 
-       @group.update(group_params)
+     @group.update(group_params)
 
-       redirect_to groups_path, notice: "Update Success"
-     end
+     redirect_to groups_path, notice: "Update Success"
+   end
 
       def destroy
         @group = Group.find(params[:id])
@@ -36,7 +36,7 @@ class GroupsController < ApplicationController
         flash[:alert] = "Group deleted"
         redirect_to groups_path
       end
-      
+
 
   private
 
